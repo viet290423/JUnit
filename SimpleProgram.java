5104e02f23c44509bdae5f38859f4f2157b88312
Duc Viet <viet2k3nguyen@gmail.com>
2024-04-18 17:36:23 +0700

    sua code

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -1,19 +1,10 @@
 public class SimpleProgram {
-
-    public static void main(String[] args) {
-        SimpleProgram program = new SimpleProgram();
-        program.run();
-    }
-
-    public void run() {
-        int number = 5;
-        
-        for (int i = 1; i <= number; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i + " la so chan.");
-            } else {
-                System.out.println(i + " la so le.");
-            }
+    public int number(int a) {
+        if(a % 2 == 0) {
+            return 0;
         }
-    }
+        else {
+            return 1;
+        }
+     }
 }
