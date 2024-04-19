fa5954fc63fd9c367ccdf236ffc61c91f1be35bf
Duc Viet <viet2k3nguyen@gmail.com>
2024-04-19 17:08:50 +0700

    cap nhat test

--- demo/src/test/java/TestSumCalculator.java
+++ demo/src/test/java/TestSumCalculator.java
@@ -10,5 +10,14 @@ public class TestSumCalculator {
 		SumCalculator a = new SumCalculator();
 		int result = a.add(2);
 		assertEquals(3, result);
+
+		int result2 = a.add(3);
+		assertEquals(6, result2);
+		
+		int result3 = a.add(4);
+		assertEquals(10, result3);
+		
+		int result4 = a.add(10);
+		assertEquals(55, result4);
 	}
 }
